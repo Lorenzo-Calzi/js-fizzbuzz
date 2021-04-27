22bba510841a3a3b8a02548f22dd7e26835d0c3e
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-04-27 15:40:27 +0200

    Correzione delle condizioni nel ciclo for

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -1,29 +1,45 @@
 //un programma che stampi i numeri da 1 a 100
 var number = 0;
 
-var name_1;
-var name_2;
-var name_3;
-
 for(i = 1; i <= 100; i++){
     var number = i;
-
-    if( number % 3 === 0){
-        number = 'Fizz';
+    document.getElementById('print').innerHTML = number;
+    if( number % 3 === 0 && number % 5 === 0 ){
+        number = 'FizzBuzz';
         console.log(number);
 
     } else if (number % 5 === 0){
         number = 'Buzz';
         console.log(number);
-
-    } else if ( number % 3 === 0 && number % 5 === 0 ){
-        number = 'FizzBuzz';
+    
+    } else if (number % 3 === 0){
+        number = 'Fizz';
         console.log(number);
 
     } else{
         console.log(number);
+        document.getElementById('print').innerHTML = number;
     }
 }
+
+
+
+
+/* if( number % 3 === 0){
+    number = 'Fizz';
+    console.log(number);
+
+} else if (number % 5 === 0){
+    number = 'Buzz';
+    console.log(number);
+
+} else if ( number % 3 === 0 && number % 5 === 0 ){
+    number = 'FizzBuzz';
+    console.log(number);
+
+} else{
+    console.log(number);
+} */
 
 
 //per i multipli di 3 stampi “Fizz” al posto del numero
